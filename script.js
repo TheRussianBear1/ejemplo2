fb24407d40f6f8ae0ca96d841122ebc56f7bff39
TheRussianBear1 <126036287+TheRussianBear1@users.noreply.github.com>
2026-05-05 15:26:04 -0400

    Add alert display functions to script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+ <script>
+    function mostrarAlerta() {
+      document.getElementById("miAlerta").style.display = "block";
+    }
+
+    function cerrarAlerta() {
+      document.getElementById("miAlerta").style.display = "none";
+    }
+  </script>
